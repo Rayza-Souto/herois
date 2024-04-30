abd73fa6755db5c177d46cc90b3725a0f0575847
Rayza Souto <rayzasouto@outlook.com>
2024-04-30 15:52:52 -0300

    Tentando criar as rotas para quadrinhos e eventos (alterar a api)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/details/?/:name" element={<Details />} />
+      <Route path="/details/:name" element={<Details />} />
       <Route path="/comics/:name" element={<Details />} />
     </Routes>
   </BrowserRouter>

63abd6573470748aa9827350a925d36cbbd7cc6b
Rayza Souto <rayzasouto@outlook.com>
2024-05-06 08:44:15 -0300

    Feat: Página comics exibindo infromações

--- src/Pages/Comics/App.js
+++ src/Pages/Comics/App.js
@@ -1,29 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
-import { BaseURL, publicKey, URLComics, timestamp, hash } from "../../Config/Key";
+import {publicKey, URLComics, timestamp, hash } from "../../Config/Key";
 import "./App.css";
 
 function ComicsDetails() {
-  const { id, name } = useParams();
-  const [comics, setComics] = useState([]);
-  const [character, setCharacter] = useState([]);
+  const { id } = useParams();
+  const [comics, setComics] = useState([]); //pegando os dados dos quadrinhos
+  //const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-    Axios.get(`${URLComics}${id}/comics?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`) //requisitando a API para pegar os detalhes do personagem
+    Axios.get(`${URLComics}${id}/comics?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`) //requisitando a API para pegar os detalhes dos quadrinhos
     .then(response => {
       const responseData = response.data; // Obtém a resposta completa
       if (responseData && responseData.data && responseData.data.results) {
-        const {id, title, description, text, thumbnail } = responseData.data.results[19];
-        const character = {
+        const {id, title, description, thumbnail } = responseData.data.results[19];
+        const comics = {
           id,
           title: title,
           description: description,
-          text: text,
           thumbnail: `${thumbnail.path}.${thumbnail.extension}`
         };
-        setCharacter(character);
-        console.log(character);
+        setComics(comics); //
+        console.log('Dados dos personagens:', comics);
       }
   })
     .catch(error => {
@@ -36,15 +35,11 @@ function ComicsDetails() {
     <div>
       <div className="details">
         <h1>Comics</h1>
-        {comics.map(comic => (
-          <div key={comic.id}>
-            <h2>{comic.title}</h2>
-            <span className="desc">Text: {comic.text}</span>
-            <span className="desc">Description: {comic.description}</span>
-            <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} className="imagem" alt={comic.title} />
-            <span className="desc">Description: {comic.description}</span>
+          <div >
+            <h2>{comics.title}</h2>
+            <img src={`${comics.thumbnail}`} className="imagem" alt="." />
+            <span className="desc">Description: {comics.description}</span>
           </div>
-        ))}
         <Link to={`/`}><button className="btn btn-outline-danger">Go Back</button></Link>
       </div>
     </div>

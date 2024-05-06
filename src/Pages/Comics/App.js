d15fbbc4118b97fb0ab201e71285e13eda931d63
Rayza Souto <rayzasouto@outlook.com>
2024-05-06 10:07:40 -0300

    feat: quadrinhos sendo apresentados na pagina de comics

--- src/Pages/Comics/App.js
+++ src/Pages/Comics/App.js
@@ -12,38 +12,40 @@ function ComicsDetails() {
   useEffect(() => {
     Axios.get(`${URLComics}${id}/comics?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`) //requisitando a API para pegar os detalhes dos quadrinhos
     .then(response => {
+      console.log(response.data);
       const responseData = response.data; // Obtém a resposta completa
       if (responseData && responseData.data && responseData.data.results) {
-        const {id, title, description, thumbnail } = responseData.data.results[19];
-        const comics = {
-          id,
-          title: title,
-          description: description,
-          thumbnail: `${thumbnail.path}.${thumbnail.extension}`
-        };
-        setComics(comics); //
-        console.log('Dados dos personagens:', comics);
+        const comicsData = responseData.data.results.map(result => ({
+          id: result.id,
+          title: result.title,
+          description: result.description,
+          thumbnail: `${result.thumbnail.path}.${result.thumbnail.extension}`
+        }));
+        setComics(comicsData);
+      } else {
+        console.error('Nenhum resultado encontrado ou estrutura de dados inesperada:', responseData);
       }
   })
-    .catch(error => {
-      console.error('Erro ao buscar os dados:', error);
-    });
-  }, [id]);
+  }, [id]); //id foi passado como dependencia para que toda vez que ele mudar a requisição seja feita novamente
   
 
   return (
     <div>
       <div className="details">
         <h1>Comics</h1>
-          <div >
-            <h2>{comics.title}</h2>
+        {comics.map(comics => (
+          <div className="card">
             <img src={`${comics.thumbnail}`} className="imagem" alt="." />
-            <span className="desc">Description: {comics.description}</span>
+            <div className="card-body">
+              <h5 className="card-title">{comics.title}</h5>
+              <p className="card-text">{comics.description}</p>
+            </div>
+          </div>
+          ))}
           </div>
         <Link to={`/`}><button className="btn btn-outline-danger">Go Back</button></Link>
       </div>
-    </div>
-  );
-};
+);
+}
 
 export default ComicsDetails;
